b5f34e2944433ed13ca9dba13f0026ff275b09e2
test-q <sr.test85@gmail.com>
2020-10-18 16:15:23 +0530

    update

--- src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
+++ src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
@@ -1,5 +1,8 @@
 package com.qa.api.gorest.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -14,11 +17,14 @@ public class CreateUserTest {
 	String domainUrl = "https://gorest.co.in";
 	String serviceUrl = "/public-api/users";
 	String token = "0ea226be7a379cc3f7951c9841679e1b584bcb4e3601914b1626b444726ee655";
+	Map<String, String> tokenMap = new HashMap<String, String>();
 	
 	@Test
 	public void createSingleUser_Test() {
+		tokenMap.put("Authorization", "Bearer " + token);
+		
 		CreateUser userPojo_Obj = new CreateUser("Vinita", "vini12@gmail.com", "Female", "Inactive");
-		Response response = RestClient.doPost(domainUrl, serviceUrl, token, "JSON", null, userPojo_Obj, true);
+		Response response = RestClient.doPost(domainUrl, serviceUrl, tokenMap, "JSON", null, userPojo_Obj, true);
 		
 		System.out.println("Status Code: " + response.getStatusCode());
 		Assert.assertEquals(response.getStatusCode(), 200);
@@ -33,8 +39,10 @@ public class CreateUserTest {
 	
 	@Test(dataProvider = "getUserData")
 	public void createUser_WithDataProvider_Test(String name, String email, String gender, String status) {
+		tokenMap.put("Authorization", "Bearer " + token);
+		
 		CreateUser userPojo_Obj = new CreateUser(name, email, gender, status);
-		Response response = RestClient.doPost(domainUrl, serviceUrl, token, "JSON", null, userPojo_Obj, true);
+		Response response = RestClient.doPost(domainUrl, serviceUrl, tokenMap, "JSON", null, userPojo_Obj, true);
 		
 		System.out.println("Status Code: " + response.getStatusCode());
 		Assert.assertEquals(response.getStatusCode(), 200);
